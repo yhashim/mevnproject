6c0c280d8f8f2b4744878c625a9b598c8b5f808e
yhashim <36962808+yhashim@users.noreply.github.com>
2020-04-09 09:40:50 -0400

    Debugging, fixing functionality

--- mevnproject/src/components/ViewPost.vue
+++ mevnproject/src/components/ViewPost.vue
@@ -10,50 +10,9 @@
         <router-link to="/posts" class="btn btn-info">View Other Posts</router-link>
       </div>
       <div class="card-footer text-muted">
-        2 days ago
+        Post by: {{ Post.poster }}
       </div>
     </div>
-
-    <h3>{{ Post.title }}</h3>
-    <small>Post by: {{ Post.poster }}</small>
-    <br/>
-    <small>{{ Post.text }}</small>
-    <br/><br/>
-
-    <h5>Add a reply:</h5>
-    <!-- <form @submit.prevent="addReply">
-      <div class="form-row">
-        <div class="form-row">
-          <label for="poster">Author</label>
-          <input type="text" id="poster" placeholder="Author" class="form-control" v-model="Reply.poster" name="poster" required>
-        </div>
-        <br/><br>
-        <div class="form-row">
-          <label for="text">Text</label>
-          <input type="text" class="form-control" id="text" aria-describedby="textHelp" placeholder="Enter text here..." name="text" v-model="Reply.text" required>
-        </div>
-      </div>
-      <br/>
-      <button type="submit" class="btn btn-light">Submit</button>
-    </form> -->
-
-    <br/><br/>
-    <h5>Replies to: {{ Post.title }}</h5>
-<!--     <table class="table table-hover">
-      <thead>
-        <tr>
-        <th>Author</th>
-        <th>Reply</th>
-      </tr>
-      </thead>
-      <tbody>
-          <tr v-for="Reply in Replies" :key="Reply._id">
-            <td>{{ Reply.poster }}</td>
-            <td>{{ Reply.text }}</td>
-          </tr>
-      </tbody>
-    </table> -->
-
   </div>
 </template>
 
@@ -66,8 +25,12 @@
       }
     },
     created() {
-      let uri = 'http://localhost:4000/Posts';
-      this.axios.get(uri).then(response => {
+      let uri = `http://localhost:4000/Posts/edit/${this.$route.params.id}`;
+        this.axios.get(uri).then((response) => {
+            this.Post = response.data;
+      });
+      let uri1 = 'http://localhost:4000/Posts';
+      this.axios.get(uri1).then(response => {
         this.Posts = response.data;
       });
     },
